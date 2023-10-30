8ac0b37a557a10f908e838c6ad5f1ad073132627
robel <robelwon75@gmail.com>
2023-10-30 20:53:22 +0300

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-# for preparing the web server for the deployment
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-service nginx restart
+# for setting up the web server for the webstatic
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<h1> Hello my world</h1>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
